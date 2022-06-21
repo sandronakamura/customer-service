5b70445301785fdac9624c7e6f7b27d0a44a6585
Sandro Nakamura <sandroarcadia@gmail.com>
2022-06-21 11:27:17 -0300

    Included: List all clients, List by id, update client by id and delete client by id

--- src/routes/client/client.js
+++ src/routes/client/client.js
@@ -1,45 +1,82 @@
-const express = require('express');
-const Client = require('../../model/client')
-const bcrypt = require('bcrypt');
-const cfg = require('../../config/config')
+const express = require("express");
+const Client = require("../../model/client");
+const bcrypt = require("bcrypt");
+const cfg = require("../../config/config");
 
 const route = express.Router();
 
-
 route.get("/", (req, res) => {
-   
-    Client.find((error, data) => {
-        if (error) return res.status(500).send({ output: `Error: ${error}`});
-        res.status(200).send({ output: "ok", data: data});
-        
-        
+  Client.find((error, data) => {
+    if (error) return res.status(500).send({ output: `Error: ${error}` });
+    res.status(200).send({ output: "ok", data: data });
+  });
+});
 
-    })
-    
+route.get("/:id", (req, res) => {
+  Client.findById(req.params.id, (error, data) => {
+    if (error) return res.status(500).send({ output: `Error: ${error}` });
+    res.status(200).send({ output: "ok", data: data });
+  });
 });
 
 route.post("/registration", (req, res) => {
-    bcrypt.hash(req.body.password, cfg.salt, (error, result) => {
-        if (error) return res.status(500).send({ output: `Erro ao gerar hash senha: ${error}`});
-        req.body.password = result;
+  bcrypt.hash(req.body.password, cfg.salt, (error, result) => {
+    if (error)
+      return res
+        .status(500)
+        .send({ output: `Erro ao gerar hash senha: ${error}` });
+    req.body.password = result;
 
+    const data = new Client(req.body);
 
-        const data = new Client(req.body);
-        
-        data.save().then((result) => {
-            res.status(201).send({ output: "Cadastro realizado: ", payload: result });
-        }).catch((error) => {
-            res.status(500).send({ output: `Erro ao cadastrar: ${error}`});
-        });
-    });  
+    data
+      .save()
+      .then((result) => {
+        res
+          .status(201)
+          .send({ output: "Cadastro realizado: ", payload: result });
+      })
+      .catch((error) => {
+        res.status(500).send({ output: `Erro ao cadastrar: ${error}` });
+      });
+  });
 });
 
 route.put("/update/:id", (req, res) => {
-    res.status(202).send({ output: "ok"})
+  if (req.body.password) {
+    bcrypt.hash(req.body.password, cfg.salt, (error, result) => {
+      if (error)
+        return res
+          .status(500)
+          .send({ output: `Erro ao gerar hash senha: ${error}` });
+      req.body.password = result;
+
+      Client.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true },
+        (error, data) => {
+          if (error)
+            return res
+              .status(500)
+              .send({ output: `Erro na atualização: ${error}` });
+          if (!data)
+            return res
+              .status(400)
+              .send({ output: `Não foi possivel atualizar: ${error}` });
+          res.status(202).send({ output: `Atualizado`, payload: data });
+        }
+      );
+    });
+  }
 });
 
 route.delete("/delete/:id", (req, res) => {
-    res.status(204).send({ output: "ok"})
+  Client.findByIdAndDelete(req.params.id, (error, data) => {
+    if (error)
+      return res.status(500).send({ output: `Erro ao deletar: ${error}` });
+    res.status(204).send({});
+  });
 });
 
 module.exports = route;
